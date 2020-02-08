9d3a8baf594804e0f9b3c7a1c2545e8e4dbdf5d9
borkarsaish65 <borkarsaish7@gmail.com>
2020-02-08 22:18:18 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -1,21 +1,36 @@
+const path = require('path');
 const express = require('express');// express is a function
+
+// console.log(__dirname);
+// console.log(path.join(__dirname,'../public'));
+//directory setup
 
 const app = express();
 
-app.get('',(req,res)=>{
-    res.send('Hello express!');
-})
+app.use(express.static(path.join(__dirname,'../public')))
 
-app.get('/help',(req,res)=>{
-    res.send('Help page'); 
-})
+// app.get('',(req,res)=>{
+//     res.send('<h1>Hello express!</h1>');
+// })
 
-app.get('/about',(req,res)=>{
-    res.send('Created by Saish Borkar');
-})
+
+// app.get('/help',(req,res)=>{
+//     res.send([{
+//         name:'Saish',
+//         age:22
+//     },{
+//         name:'Nikhil',age:14
+//     }]); 
+// })
+
+// app.get('/about',(req,res)=>{
+//     res.send('<h1>Created by Saish Borkar</h1>');
+// })
 
 app.get('/weather',(req,res)=>{
-    res.send('Weather App'); 
+    res.send({latitude:15.232,
+               longitude:72.213,
+            location:'Margao'}); 
 })
 
 app.listen(3000,()=>{
