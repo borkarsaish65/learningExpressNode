49ca289a6fe1ec320cf656cd488dda034361410b
borkarsaish65 <borkarsaish7@gmail.com>
2020-02-08 22:59:36 +0530

    third commit

--- src/app.js
+++ src/app.js
@@ -7,7 +7,14 @@ const express = require('express');// express is a function
 
 const app = express();
 
+//define paths for express config
+const viewsPath = path.join(__dirname,'../templates');
 app.use(express.static(path.join(__dirname,'../public')))
+
+//setup handlebars engine and views location
+app.set('view engine','hbs');
+app.set('views',viewsPath);
+
 
 // app.get('',(req,res)=>{
 //     res.send('<h1>Hello express!</h1>');
@@ -23,9 +30,26 @@ app.use(express.static(path.join(__dirname,'../public')))
 //     }]); 
 // })
 
-// app.get('/about',(req,res)=>{
-//     res.send('<h1>Created by Saish Borkar</h1>');
-// })
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        companyName:'Microsoft'
+    });
+})
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather App',
+        name:'Saish Borkar'
+    });
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        msg:'We are happy to help you'
+    })
+})
+
+
 
 app.get('/weather',(req,res)=>{
     res.send({latitude:15.232,
