747ce70de39917dc33090615b5163693056a074d
borkarsaish65 <borkarsaish7@gmail.com>
2020-02-09 10:45:43 +0530

    latest commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');// express is a function
-
+const hbs = require('hbs');
 // console.log(__dirname);
 // console.log(path.join(__dirname,'../public'));
 //directory setup
@@ -8,13 +8,15 @@ const express = require('express');// express is a function
 const app = express();
 
 //define paths for express config
-const viewsPath = path.join(__dirname,'../templates');
+const viewsPath = path.join(__dirname,'../templates/views');
+const partialsPath = path.join(__dirname,'../templates/partials');
 app.use(express.static(path.join(__dirname,'../public')))
 
 //setup handlebars engine and views location
+
 app.set('view engine','hbs');
 app.set('views',viewsPath);
-
+hbs.registerPartials(partialsPath);
 
 // app.get('',(req,res)=>{
 //     res.send('<h1>Hello express!</h1>');
@@ -32,6 +34,8 @@ app.set('views',viewsPath);
 
 app.get('/about',(req,res)=>{
     res.render('about',{
+        title:'About',
+        name:'saish',
         companyName:'Microsoft'
     });
 })
@@ -45,7 +49,9 @@ app.get('',(req,res)=>{
 
 app.get('/help',(req,res)=>{
     res.render('help',{
-        msg:'We are happy to help you'
+        msg:'We are happy to help you',
+        title:'help',
+        name:'Saish Borkar'
     })
 })
 
@@ -56,6 +62,25 @@ app.get('/weather',(req,res)=>{
                longitude:72.213,
             location:'Margao'}); 
 })
+
+app.get('/help/*',(req,res)=>{
+
+    res.render('404',{
+        desc:'Help Article not found',
+        name:'saish'
+    });
+})
+
+
+
+app.get('*',(req,res)=>{
+
+    res.render('404',{
+        desc:'Page not found',
+        name:'saish'
+    })
+})
+
 
 app.listen(3000,()=>{
     console.log('Server is up on port 3000');
